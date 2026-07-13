abb447f90d8b5d210ee31d7d1b5779dd8987b66f
Sabine <sabine.draeger_contractor@celanese.com>
2026-07-13 08:22:10 -0300

    Organiza evidencias e comandos Cypress

--- cypress/e2e/admin-books.cy.js
+++ cypress/e2e/admin-books.cy.js
@@ -1,63 +1,6 @@
 const { faker } = require('@faker-js/faker');
 const adminBooksPage = require('../pages/AdminBooksPage');
 
-const removerLivrosTemporarios = () => {
-  cy.window().then((win) => {
-    const token = win.localStorage.getItem('authToken');
-
-    cy.request({
-      method: 'GET',
-      url: '/api/books?search=Livro&limit=100',
-      headers: { Authorization: token },
-      log: false,
-    }).then(({ body }) => {
-      const livrosTemporarios = (body.books || []).filter((book) => {
-        const ehAutorTeste = book.author && book.author.startsWith('Autora Teste QA');
-        const ehTituloTeste = book.title && book.title.startsWith('LivroTesteAdmin');
-
-        return ehAutorTeste || ehTituloTeste;
-      });
-
-      cy.wrap(livrosTemporarios).each((book) => {
-        cy.request({
-          method: 'DELETE',
-          url: `/api/books/${book.id}`,
-          headers: { Authorization: token },
-          failOnStatusCode: false,
-          log: false,
-        });
-      });
-    });
-  });
-};
-
-const criarLivroPorApi = (livro) => {
-  cy.window().then((win) => {
-    const token = win.localStorage.getItem('authToken');
-
-    cy.request({
-      method: 'POST',
-      url: '/api/books',
-      headers: { Authorization: token },
-      log: false,
-      body: {
-        title: livro.titulo,
-        author: livro.autor,
-        description: livro.descricao,
-        category: livro.categoria,
-        isbn: livro.isbn,
-        editor: livro.editora,
-        language: livro.idioma,
-        publication_year: livro.ano,
-        pages: livro.paginas,
-        total_copies: livro.exemplares,
-        available_copies: livro.exemplares,
-        cover_image: livro.capa,
-      },
-    }).its('status').should('eq', 201);
-  });
-};
-
 describe('Gerenciamento de livros', () => {
   beforeEach(() => {
     cy.encerrarSessao();
@@ -66,12 +9,12 @@ describe('Gerenciamento de livros', () => {
       const admin = positivos.find((usuario) => usuario.redirecionamento === '/admin-dashboard.html');
 
       cy.loginUsuario(admin);
-      removerLivrosTemporarios();
+      cy.removerLivrosTemporarios();
     });
   });
 
   afterEach(() => {
-    removerLivrosTemporarios();
+    cy.removerLivrosTemporarios();
   });
 
   it('deve criar, visualizar, editar e excluir um livro', () => {
@@ -96,7 +39,7 @@ describe('Gerenciamento de livros', () => {
         capa: edicao.capa,
       };
 
-      criarLivroPorApi(livro);
+      cy.criarLivroPorApi(livro);
 
       adminBooksPage.acessar();
       adminBooksPage.validarPagina();
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -55,3 +55,60 @@ Cypress.Commands.add('encerrarSessao', () => {
   cy.clearLocalStorage();
   cy.clearCookies();
 });
+
+Cypress.Commands.add('removerLivrosTemporarios', () => {
+  cy.window().then((win) => {
+    const token = win.localStorage.getItem('authToken');
+
+    cy.request({
+      method: 'GET',
+      url: '/api/books?search=Livro&limit=100',
+      headers: { Authorization: token },
+      log: false,
+    }).then(({ body }) => {
+      const livrosTemporarios = (body.books || []).filter((book) => {
+        const ehAutorTeste = book.author && book.author.startsWith('Autora Teste QA');
+        const ehTituloTeste = book.title && book.title.startsWith('LivroTesteAdmin');
+
+        return ehAutorTeste || ehTituloTeste;
+      });
+
+      cy.wrap(livrosTemporarios, { log: false }).each((book) => {
+        cy.request({
+          method: 'DELETE',
+          url: `/api/books/${book.id}`,
+          headers: { Authorization: token },
+          failOnStatusCode: false,
+          log: false,
+        });
+      });
+    });
+  });
+});
+
+Cypress.Commands.add('criarLivroPorApi', (livro) => {
+  cy.window().then((win) => {
+    const token = win.localStorage.getItem('authToken');
+
+    cy.request({
+      method: 'POST',
+      url: '/api/books',
+      headers: { Authorization: token },
+      log: false,
+      body: {
+        title: livro.titulo,
+        author: livro.autor,
+        description: livro.descricao,
+        category: livro.categoria,
+        isbn: livro.isbn,
+        editor: livro.editora,
+        language: livro.idioma,
+        publication_year: livro.ano,
+        pages: livro.paginas,
+        total_copies: livro.exemplares,
+        available_copies: livro.exemplares,
+        cover_image: livro.capa,
+      },
+    }).its('status').should('eq', 201);
+  });
+});
